a3443ac2a4af7b98457a8875eb47d6cfab712aae
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-08-03 07:50:06 +0600

    added some codes

---
+++ Trimester-3/CSE 1115-OOP/Codes/ClassWork/src/Sum.java
@@ -0,0 +1,20 @@
+public class Sum {
+    private int x;
+    private int y;
+    private int z;
+
+    public Sum(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getSum() {
+        return x + y + z;
+    }
+
+    @Override
+    public String toString() {
+        return "Sum: " + getSum();
+    }
+}
