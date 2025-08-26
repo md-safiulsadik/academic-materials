fbb069c32e955dbbba2aaf028cdd07cdcd5ebb69
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-08-26 11:55:02 +0600

    added lab-5

---
+++ Trimester-3/CSE 1116-OOP (lab)/Codes/lab-5/src/Calculator.java
@@ -0,0 +1,17 @@
+
+public class Calculator {
+
+    public void sum() {
+        System.out.println("This is 1st sum: ");
+    }
+
+    public void sum(int x, int y) {
+        System.out.println("This is 2nd sum: " + x + y);
+    }
+
+    public void sum(float x, float y) {
+        System.out.println("This is 3rd sum: " + x+y);
+    }
+}
+
+
---
+++ Trimester-3/CSE 1116-OOP (lab)/Codes/lab-5/src/TestClass.java
@@ -0,0 +1,13 @@
+
+public class TestClass {
+
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        calculator.sum();
+        calculator.sum(3, 2);
+        calculator.sum(3.6f, 4.5f);
+
+    }
+}
