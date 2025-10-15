88e59bb0def2d1fbf2584b6593282001c358f020
md-safiulsadik <209575811+md-safiulsadik@users.noreply.github.com>
2025-10-15 12:48:59 +0600

    new assignment added

--- Trimester-3/CSE 1115-OOP/BonusAssignment02/src/Cow.java
+++ Trimester-3/CSE 1115-OOP/BonusAssignment02/src/Cow.java
@@ -3,5 +3,6 @@ public class Cow extends Animal{
     @Override
     public void makeSound() {
         System.out.println("Cow goes moo-moo");
+        System.out.println(getClass().getName());
     }
 }
